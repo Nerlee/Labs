a47ca6bd84e684dc8d6e03b3aca43b5231c81615
Nerlee <dasada075@yandex.ru>
2026-03-17 20:29:04 +0700

    Lab1

---
+++ Anton1/Anton1/Lesson.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anton1
+{
+    public class Lesson
+    {
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public string Name { get; set; }
+
+        public void ShowInform()
+        {
+            Console.WriteLine($"Дата занятия: {Date.Day}.{Date.Month}.{Date.Year}");
+            Console.WriteLine($"Время занятия: {Time.Hours}:{Time.Minutes}");
+            Console.WriteLine($"Имя преподавателя: {Name}");
+        }
+
+        public Lesson(DateTime date, TimeSpan time, string name) 
+        {
+            Date = date;
+            Time = time;
+            Name = name;
+        }  
+        ~Lesson() { }
+    }
+}
---
+++ Anton1/Anton1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Anton1
+{
+    internal class Program
+    {
+        static string trueString(string s)
+        {
+            string[] strings = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string result = "";
+            for (int i = 0; i < strings.Length; i++)
+            {
+                result += strings[i];
+                if (i != strings.Length - 1) result += " ";
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            //string rawString = Console.ReadLine();
+            string rawString = "12-12-12       23:50     \"Steve\"";
+            List<string> list = new List<string>();
+            string[] words = rawString.Split(new char[] { '"' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in words)
+            {
+                string[] subword = word.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (subword.Length == 1) list.Add(word);
+                else if (subword.Length == 2) list.AddRange(subword);
+            }
+
+            string workDate = trueString(list[0]);
+            string timedate = trueString(list[1]);
+            string name = trueString(list[2]);
+
+            DateTime dateOfWork = DateTime.Parse(workDate);
+
+            TimeSpan timeSpan;
+
+            if (TimeSpan.TryParse(timedate, out timeSpan))
+            {
+                Lesson lesson = new Lesson(dateOfWork, timeSpan, name);
+                lesson.ShowInform();
+            }
+            else
+            {
+                Console.WriteLine("Неверный формат времени.");
+            }
+        }
+    }
+}
